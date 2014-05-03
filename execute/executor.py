f4a36c5281f91fd562ff8d8cf3577f8e8bda5d50
arnabm <arnabm@berkeley.edu>
2014-05-02 19:53:02 -0700

    updated executor with text functionality
    Can text now

--- execute/executor.py
+++ execute/executor.py
@@ -1,4 +1,5 @@
 import smtplib
+from email.mime.text import MIMEText
 
 class executor:
     def __init__(self, argument, data):
@@ -11,20 +12,29 @@ class executor:
 
 class emailer:
     def __init__(self, data):
-        self.username = ''
-        self.password = ''
-        self.fromaddr = ''
-        self.toaddrs = ''
+        self.username = 'username'
+        self.password = 'password'
+        self.fromaddr = 'username@gmail.com'
+        self.toaddrs = 'username@domain.com'
         self.msg = data
     def run(self):
         server = smtplib.SMTP('smtp.gmail.com:587')
         server.ehlo()
         server.starttls()
         server.login(self.username,self.password)
-        server.sendmail(self.fromaddr, self.toaddrs, self.msg)
+        server.sendmail(self.fromaddr, self.toaddrs, self.msg.as_string())
         server.quit()
 class texter:
     def __init__(self, data):
-        self.msg = data
+        self.username = 'username'
+        self.password = 'password'
+        self.fromaddr = 'username@gmail.com'
+        self.toaddrs = 'phonenumber@vtext.com'
+        self.msg = MIMEText(str(data))
     def run(self):
-        print self.msg
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(self.username,self.password)
+        server.sendmail(self.fromaddr, self.toaddrs, self.msg.as_string())
+        server.quit()
